0f3d4e899ccc4ef125bd03b24cf63b7ea6a892b3
jak-sys <camalajakner42@gmail.com>
2024-11-20 20:26:04 -0500

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+Array.from(document.getElementsByTagName('input')).forEach((e,i)=>{
+    e.addEventListener('keyup', (el)=>{
+        if (e.value.length > 0) {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.color = "gray"
+        } else {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.color = "gray"
+        }
+    })
+})
+
+Array.from(document.getElementsByTagName('input')).forEach((e,i)=>{
+    e.addEventListener('keyup', (el)=>{
+        if (e.value.length > 0) {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)"
+        } else {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)"
+        }
+    })
+})
